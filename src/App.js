101700886ae2c45188f2b4b120065a74d7bf32ac
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-05-18 07:54:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Sign_Up from './components/Sign_Up';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Dll sca vjhbjh </h1>
+<Sign_Up />
     </div>
   );
 }
---
+++ src/components/Sign_Up.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import './landing.scss'
+
+const Sign_Up = () => {
+  return (
+    <div className="sign-up">Sign_Up</div>
+  )
+}
+
+export default Sign_Up
